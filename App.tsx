238ec05d878ba55eced63b827ee15398c6493813
Erik R. Yverling <erik.yverling@storytel.com>
2023-05-15 15:22:05 +0200

    Add basic example of props, styles and hook

--- App.tsx
+++ App.tsx
@@ -1,118 +1,73 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+import React, {Component} from 'react';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+    container: {
+        alignItems: 'center',
+        padding: 30,
+        gap: 30
+    },
+    button: {
+        alignItems: 'center',
+        backgroundColor: '#4f99ad',
+        padding: 15,
+        marginBottom: 15,
+    },
+    buttonText: {
+        color: '#e6e6e6',
+    }
 });
+
+type GreetingProps = {
+    name: string;
+}
+
+// Functional component
+const Greeting = (props: GreetingProps) => {
+    return (
+        <View style={styles.center}>
+            <Text>Hello, {props.name}!</Text>
+        </View>
+    );
+};
+
+// Class component
+class Counter extends Component {
+
+    // Hook
+    state = { count: 0, };
+
+    onPress = () => {
+        this.setState({
+            count: this.state.count + 1,
+        });
+    };
+
+    render() {
+        return(
+            <View>
+                <TouchableOpacity style={styles.button} onPress={this.onPress}>
+                    <Text style={styles.buttonText}>Click me</Text>
+                </TouchableOpacity>
+                <View>
+                    <Text>You clicked {this.state.count} times</Text>
+                </View>
+            </View>
+        );
+    }
+}
+
+class App extends Component {
+    render() {
+        return(
+            <View style={[styles.container]}>
+                <Greeting name="Cloud"/>
+                <Greeting name="Tifa"/>
+                <Greeting name="Aerith"/>
+                <Counter/>
+            </View>
+        );
+    }
+};
 
 export default App;
